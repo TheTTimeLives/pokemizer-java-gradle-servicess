b354130ebfc6ceceeddaf940682ccb179fcf9b65
Taylor M <melnikprod@gmail.com>
2023-02-15 14:33:22 -0500

    Frontends

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage  from "./components/pages/LandingPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Using elements in route is better than stating component. Prevents writing extra useEffect handling within component to avoid accidental restarting react lifecycle hooks which can cause duplicate components by the router being created each time the route is hit. */}
+          <Route exact path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
